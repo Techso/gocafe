66583aa9283102f65d424612ecb68604d3c2d3cc
Techso <stevenlarex@gmail.com>
2020-03-26 21:04:57 +0100

    first commit

---
+++ build/precache-manifest.26e53b2b4b7da0f096cf2b464bf8b061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670045424d44d0a3994f20de6c535d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64485bf5f0904f7c0a63",
+    "url": "/static/css/main.814527fa.chunk.css"
+  },
+  {
+    "revision": "7786172f99011904c388",
+    "url": "/static/js/2.71c7aedf.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.71c7aedf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64485bf5f0904f7c0a63",
+    "url": "/static/js/main.71bd6d0d.chunk.js"
+  },
+  {
+    "revision": "1aac0dbf4d2711e9a2ba",
+    "url": "/static/js/runtime-main.1d8313c0.js"
+  }
+]);
